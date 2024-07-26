351738566789d2a60569f5018dcbf5022ff9146c
Gustavoaaaaaa <129516091+Gustavoaaaaaa@users.noreply.github.com>
2024-07-26 10:43:24 -0300

    Add files via upload

--- BETA/main.js
+++ BETA/main.js
@@ -15,10 +15,12 @@ for (let i = 0; i < botoes.length; i++) {
 }
 
 const contadores = document.querySelectorAll(".contador");
-const tempoObjetivo1 = new Date("4029-09-23T00:00:00");
-let tempoAtual = new Date();
+const tempoObjetivo1 = new Date("2020-10-05T00:00:00");
+const tempoObjetivo2 = new Date("2023-12-05T00:00:00");
+const tempoObjetivo3 = new Date("2023-12-30T00:00:00");
+const tempoObjetivo4 = new Date("2024-02-01T00:00:00");
 
-contadores[0].textContent = calculaTempo(tempoObjetivo1);
+const tempos = [tempoObjetivo1, tempoObjetivo2, tempoObjetivo3, tempoObjetivo4];
 
 function calculaTempo(tempoObjetivo1) {
     let tempoAtual = new Date();
@@ -31,6 +33,22 @@ function calculaTempo(tempoObjetivo1) {
     segundos %= 60;
     minutos %= 60;
     horas %= 24;
-
+if (tempoFinal > 0){
     return dias + " dias " + horas + " horas " + minutos + " minutos " + segundos + " segundos";
+} else {
+    return "Prazo Finalizado";
 }
+}
+
+function atualizaCronometro(){
+    for (let i=0; i<contadores.length;i++){
+        contadores[i].textContent = calculaTempo(tempos[1]);
+    }
+}
+
+function comecaCronometro(){
+    atualizaCronometro();
+    setInterval(atualizaCronometro,1000);
+}
+
+comecaCronometro();
